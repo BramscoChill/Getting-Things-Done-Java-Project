99de8f930f040874e51c8c6386aaf1ebc9883dbf
Bram <fastfoodsenator@gmail.com>
2011-10-05 13:11:00 +0200

    main normaal gemaakt

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,36 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * a
- */
+
 package controller;
 
-/**
- *
- * @author Administrator
- */
 
-
-//sjors leaves a comment
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+		
     }
-<<<<<<< HEAD
-	
-	public void Bami(){
-	}
-=======
-    
-    
-    public int testfunctie()
-    {
-        return 0;
-    }
-
->>>>>>> commit nummer 100
 }
