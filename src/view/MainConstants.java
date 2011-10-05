3b0f8adabe91b8d0a7f408014da1e9ea3cf801ba
Bram <fastfoodsenator@gmail.com>
2011-10-05 13:21:19 +0200

    constanten en dbhandler toegevoegd

---
+++ src/view/MainConstants.java
@@ -0,0 +1,16 @@
+/*
+ * Algemene constanten die in heel de applicatie gebruikt worden
+ */
+package view;
+
+/**
+ *
+ * @author Bram Klein
+ */
+public class MainConstants {
+    public static final String MYSQL_SERVER = "kleinprodesign.nl";
+    public static final String MYSQL_USERNAME = "doomrider_GTD";
+    public static final String MYSQL_PASSWORD = "xj4KAX33";
+    public static final String DB_NAME = "doomrider_GTD";
+    
+}
